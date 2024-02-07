9776f27dce923f82ba3e11b6053aae61b6697fd9
Harsh Nikharge <158061768+harshngit@users.noreply.github.com>
2024-02-07 21:09:04 +0530

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,423 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link href="http://fonts.cdnfonts.com/css/poppins" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<title>HARSH TECH</title>
+</head>
+<body>
+	<div class="header" id="top-header">
+		<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+			<div class="container text-uppercase p-2">
+				
+  <a class="navbar-brand font-weight-bold text-white" href="#"> <img src="img/logoweb.png" style="width:200px; height: 100px; position: fixed; left: 10px; top:-10px;"> </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+    <ul class="navbar-nav ml-auto text-uppercase">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#servicediv">Service</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#pricingdiv">Price</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#newsletterid">About</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#contactid">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+
+    </ul>
+  </div>
+</div>
+</nav>
+<section class="header-section">
+	<div class="center-div">
+		<h1 class="font-weight-bold"> We are The Web Developers </h1>
+		<p>We create the world best websities</p>
+		<div class="header-buttons">
+			<a href="#projectworkdiv">
+				About Us
+			</a>
+			<a href="#contactid">Contact</a>
+		</div>
+	</div>
+	
+</section>
+
+	</div>
+
+<!--*************** header part end ****************-->
+
+<!--*******************three extra header div starts*********-->
+
+<section class="header-extradiv">
+  <div class="container">
+    <div class="row">
+      <div class=" extra-div col-1g-4 col-md-4 col-12">
+        <a href="#">
+          <i class="fa-3x fa fa-desktop" aria-hidden="true"></i></a>
+          <h2>Easy To Use</h2>
+
+          <p>Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do euismod tepo raraincididunt</p>
+      </div>
+      <div class="  extra-div col-1g-4 col-md-4 col-12">
+        <a href="#">
+          <i class="fa-3x fa fa-trophy" aria-hidden="true"></i></a>
+          <h2>Awesome Design</h2>
+
+          <p>Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do euismod tepo raraincididunt</p>
+      </div>
+      <div class="  extra-div col-1g-4 col-md-4 col-12">
+        <a href="#">
+          <i class="fa-3x fa fa-magic" aria-hidden="true"></i></a>
+          <h2>Easy To Customize</h2>
+
+          <p>Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do euismod tepo raraincididunt</p>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+
+
+<!--*******************three extra header div ends*********-->
+
+<!--*******************offer section start *********-->
+ <section class="serviceoffers" id="servicediv">
+   <div class="container headings text-center">
+     <h1 class="text-center font-weight-bold"> WHAT DO WE OFFER</h1>
+     <p class="text-center"> Lorem ipsum sit amet , consectetur adipisicing elit.</p>
+   </div>
+   <div class="container">
+     <div class="row">
+       <div class="col-lg-6 col-md-12 col-10 offset-1 offset-lg-0">
+         <div class="names my-3">
+           <h1>HTML</h1>
+           <div class="progress">
+           <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" style="width:100%">100%</div>
+         </div>
+         <div class="names my-3">
+           <h1>JAVASCRIPT</h1>
+           <div class="progress">
+           <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width:85%">85%</div>
+         </div>
+         <div class="names my-3">
+           <h1>CSS</h1>
+           <div class="progress">
+           <div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" style="width:75%">75%</div>
+         </div>
+         <div class="names my-3">
+           <h1>REACTJS</h1>
+           <div class="progress">
+           <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width:80%">80%</div>
+         </div>
+         <div class="names my-3">
+           <h1>NODEJS</h1>
+           <div class="progress">
+           <div class="progress-bar progress-bar-striped progress-bar-animated bg-dark" style="width:100%">100%</div>
+         </div>
+         </div>
+       </div>
+       <div class="col-lg-6 col-md-12 col-12 servicediv">
+         <div class="container">
+           <div class="col-lg-2 col-2 service-icons">
+             <i class=" fa-3x fa fa-desktop" aria-hidden="true"></i>
+           </div>
+           <div class="col-lg-10 col-10">
+             <h2>WEBSITE DEVELOPEMENT</h2>
+             <p> Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do euismod tepo raraincididunt </p>
+           </div>
+         </div>
+         <div class="container">
+           <div class="col-lg-2 col-2 service-icons">
+             <i class=" fa-3x fa fa-wifi" aria-hidden="true"></i>
+           </div>
+           <div class="col-lg-10 col-10">
+             <h2>DIGITAL MARKETING</h2>
+             <p> Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do euismod tepo raraincididunt </p>
+           </div>
+         </div>
+         <div class="container">
+           <div class="col-lg-2 col-2 service-icons">
+             <i class=" fa-3x fa fa-phone" aria-hidden="true"></i>
+           </div>
+           <div class="col-lg-10 col-10">
+             <h2>SUPPORT 24/7</h2>
+             <p> Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do euismod tepo raraincididunt </p>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ </section>
+
+<!--*******************offer section end *********-->
+
+<!--*******************project done starts *********-->
+<section class="project-work" id="projectworkdiv">
+     <div class="container headings text-center">
+     <p class="text-center font-weight-bold"> More Than 2,000 websities created
+     </p>
+   </div>
+   <div class="container d-flex justify-content-around align-content-center text-center">
+     <div >
+       <h1 class="count">1500</h1>
+       <p>CMS installation<p>
+     </div>
+
+     <div >
+       <h1 class="count">2500</h1>
+       <p>Award Won<p>
+     </div>
+
+     <div >
+       <h1 class="count">700</h1>
+       <p>Happy Clients<p>
+     </div>
+
+     <div >
+       <h1 class="count">500</h1>
+       <p>Working on<p>
+     </div>
+        </div>
+</section>
+
+<!--*******************project done ends *********-->
+<!--*******************our pricing starts *********-->
+ <section class="pricing" id="pricingdiv">
+   <div class="container headings text-center">
+     <h1 class="text-center font-weight-bold text-white">Our Best Pricing</h1>
+   </div>
+   <div class="container">
+     <div class="row">
+       <div class="col-lg-4 col-12">
+         <div class="card">
+          <div class="card-header">Basic</div>
+           <div class="card-body">
+             <li>
+               $<span class="money">20</span>/website
+             </li>
+             <li>Responsive Websities</li>
+             <li> Domain Name Free</li>
+             <li> mobile Friendly</li>
+             <li>Webmail Support</li>
+             <li>Customer Support 24/7</li>
+           </div>
+           <div class="card-footer">
+           <a href="#">Purchase</a>
+         </div>
+        </div>
+
+       </div>
+              <div class="col-lg-4 col-12 card-second">
+         <div class="card">
+          <div class="card-header">Standard</div>
+           <div class="card-body">
+             <li>
+               $<span class="money">40</span>/website
+             </li>
+             <li>Responsive Websities</li>
+             <li> Domain Name Free</li>
+             <li> mobile Friendly</li>
+             <li>Webmail Support</li>
+             <li>Customer Support 24/7</li>
+           </div>
+           <div class="card-footer">
+           <a href="#">Purchase</a>
+         </div>
+        </div>
+
+       </div>
+              <div class="col-lg-4 col-12">
+         <div class="card">
+          <div class="card-header">Unlimited</div>
+           <div class="card-body">
+             <li>
+               $<span class="money">60</span>/website
+             </li>
+             <li>Responsive Websities</li>
+             <li> Domain Name Free</li>
+             <li> mobile Friendly</li>
+             <li>Webmail Support</li>
+             <li>Customer Support 24/7</li>
+           </div>
+           <div class="card-footer">
+           <a href="#">Purchase</a>
+         </div>
+        </div>
+
+       </div>
+     </div>
+   </div>
+ </section>
+<!--*******************our pricing ends *********-->  
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+ 
+</section -->>
+<!--*******************our customer feedback ends *********-->
+<!--*******************Contact Us starts *********-->
+<section class="contactus" id="contactid">
+   <div class="container headings text-center">
+     <h1 class="text-center font-weight-bold">Contact Us</h1>
+     <p class="text-capitalize pt-1">We're Here To Help And Answer Any Question You Might Have. We look Forward To Hearing From You 😀</p>
+   </div>
+
+   <div class="container">
+     <div class="row">
+       <div class="col-lg-8 col-md-8 col-10 offset-lg-2 offset-md-2 offset-1"> 
+
+          <form  method="POST">
+           <div class="form-group">
+            <input type="text" class="form-control" name="username" placeholder="Enter Usermame" id="Username" required autocomplete="off">
+       </div>
+
+        <form action="/action_page.php">
+           <div class="form-group">
+            <input type="text" class="form-control" name="mobile" placeholder="Enter mobile number" id="Mobile Number" required autocomplete="off">
+       </div>
+        
+         <form action="/action_page.php">
+           <div class="form-group">
+            <input type="text" class="form-control" name="email" placeholder="Enter  Email address" id="Email" required autocomplete="off">
+       </div>
+       <div class="form-group">
+           <textarea class="form-control" rows="5" name="message" id="message" placeholder="Enter your messsage"  required autocomplete="off"></textarea>
+       </div>
+        <div class="d-flex justify-content-center form-button">
+        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+        </div>
+      </form>
+       </div>
+     </div>
+   </div>
+</section>
+
+<!--*******************Contact Us ends *********-->
+<!--*******************Suscribe newesletter Starts *********-->
+<section class="newsletter" id="newsletterid">
+   <div class="container headings text-center">
+     <h1 class="text-center font-weight-bold">SUBSCRIBE TO OUR NEWSLLETTER</h1>
+   </div>
+   <div class="container">
+     <div class="row">
+      <div class="col-lg-8 offset-lg-2 col-12">
+          <div class="input-group mb-3">
+           <input type="text" class="form-control news-input" placeholder="Your Email">
+            <div class="input-group-append">
+            <span class="input-group-text">Suscribe</span>
+    </div>
+  </div>
+      </div>
+       
+     </div>
+   </div>
+ </section>
+<!--*******************Suscribe newesletter ends *********-->
+<!--*******************Footer starts *********-->
+<footer class="footersection" id="footerdiv">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-6 col-12 footer-div">
+        <div >
+          <h3>About HARSHTECH</h3>
+          <p>The world has become so fast paced that people don't want to stand by reading a page of information to be they wou;d much rather look at a presentation and understand the message .</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-12 footer-div ">
+        <div class="footersection1 text-center">
+          <h3>NAVIGATION LINKS</h3>
+          <li>
+          <a href="#">Home</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Price</a></li>
+          <li><a href="#">About</a></li>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-12 col-12 footer-div">
+        <div >
+          <h3>NEWSLETTER</h3>
+          <p>We write articles on current and Interesting Technicals Topic to spread knowledge and sometimes on Social Causes too.</p>
+
+        <div class="container mewsletter-main">
+               <div class="row">
+                  <div class="col-lg-12 col-12">
+          <div class="input-group mb-3">
+           <input type="text" class="form-control news-input" placeholder="Your Email">
+            <div class="input-group-append">
+            <span class="input-group-text">Suscribe</span>
+    </div>
+     </div>
+      </div>
+
+    </div>
+  </div>
+</footer>
+<div class="mt-5 text-center">
+      <p>Design by @codewitharsh</p>
+      <img src="img/logoweb.png" style="width:200px; height:100px;">
+    </div>
+    <div class="scrolltop float-right">
+      <i class="fa fa-arrow-up arrow" onclick="topFunction()" id="myBtn"></i>
+    </div>
+<!--*******************Footer ends *********-->
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js" integrity="sha512-CEiA+78TpP9KAIPzqBvxUv8hy41jyI3f2uHi7DGp/Y/Ka973qgSdybNegWFciqh6GrN2UePx2KkflnQUbUhNIA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js" integrity="sha512-d8F1J2kyiRowBB/8/pAWsqUl0wSEOkG5KATkVV4slfblq9VRQ6MyDZVxWl2tWd+mPhuCbpTB4M7uU/x9FlgQ9Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  <script>
+    $('.count').counterUp({
+      delay:10,
+      time:3000
+    })
+    //Get the button:
+    mybutton = document.getElementByclass("arrow")
+
+    //when the user scroll down 20px from the top the bottom of the document , show the buttom 
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body .scrollTop > 20 || document.documentElement.scrollTop > 20)  {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+
+    // Whem=n the user clicks on the button, scroll to the top document
+    function topFunction() {
+      document.body .scrollTop = 0; // Foe Safari
+      document.documentElement.scrollTop = 0;// For Chrome , Firefox , IE and Opera 
+    }
+
+  </script>
+</body>
+</html>
+
